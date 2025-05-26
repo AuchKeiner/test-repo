9fed989a28af5970f6bbddd6eb05b02402ab3760
AuchKeiner <24172800+AuchKeiner@users.noreply.github.com>
2025-05-26 21:52:58 +0200

    Create test-user.js

---
+++ test-user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-26
+// @description  try to take over the world!
+// @author       You
+// @match        *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("test")
+})();
